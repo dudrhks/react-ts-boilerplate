05f31d67bd89d2b3f948b282d5c772490119f554
Yeonggwan Han(Malone) <66952465+dudrhks@users.noreply.github.com>
2023-05-15 11:06:10 +0900

    ✨ react 보일러 템플릿생성

---
+++ src/style/GlobalStyle.tsx
@@ -0,0 +1,15 @@
+import 'the-new-css-reset/css/reset.css'
+
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Noto Sans KR', Helvetica, '맑은 고딕', 'malgun gothic', 		   
+		'Apple SD Gothic Neo', 'Apple SD 산돌고딕 Neo',  		   
+		'Microsoft NeoGothic',  		   
+		'Droid sans', sans-serif; 
+    line-height: 1.5;
+  }
+`
+
+export default GlobalStyle
